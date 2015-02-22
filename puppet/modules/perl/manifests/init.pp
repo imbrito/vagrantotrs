80a859a0b23e65d4d4656f85e49fb45efe16d735
imbrito <ismael.20rj@gmail.com>
2015-02-22 00:41:01 -0300

    #8 add perl package

--- puppet/modules/perl/manifests/init.pp
+++ puppet/modules/perl/manifests/init.pp
@@ -2,7 +2,11 @@
 
 class perl::install{
     #http://www.puppetcookbook.com/posts/install-multiple-packages.html
-    $perl = [ 'perl' ]
+    $perl = [ 'perl', 'libapache2-reload-perl', 'libarchive-zip-perl', 'libcrypt-eksblowfish-perl', 'libcrypt-ssleay-perl',
+    'libdbd-odbc-perl', 'libdbd-pg-perl', 'libencode-hanextra-perl', 'libgd-gd2-perl', 'libgd-text-perl', 'libgd-graph-perl',
+    'libio-socket-ssl-perl', 'libjson-xs-perl', 'libmail-imapclient-perl',  'libapache2-mod-perl2', 'libnet-dns-perl',
+    'libnet-ldap-perl', 'libpdf-api2-perl', 'libtemplate-perl', 'libtext-csv-xs-perl', 'libxml-parser-perl', 
+    'libyaml-libyaml-perl' ]
     
     package {$perl:
         ensure => present,
